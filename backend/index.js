72801799f5dcd79d685ae4878bc775f23d218bbf
Safeway-KelvinTezinde <kelvin.tezinde@albertsons.com>
2022-06-08 18:16:22 -0400

    changes to backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 const app = express()
-const apiPort = 8081
+const bodyParser = require("body-parser")
+
+const apiPort = 8080
 
 const userDetailRouter = require('./routes/spa-user-router')
 const db = require('./db')
@@ -10,12 +12,17 @@ const db = require('./db')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static(process.cwd()+"../simple-spa-form/build/"));
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('api')
 })
+
+app.get('/', (req,res) => {
+    res.sendFile(process.cwd()+"../simple-spa-form/build/index.html");
+});
 
 app.use('/api', userDetailRouter)
 
